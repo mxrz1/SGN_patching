078d0336ecdf9058851e2e308760aa9b35081192
Butter <butterman423@gmail.com>
2024-11-25 04:50:43 +0000

    Removed unused files

--- install.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash
-
-source .venv/bin/activate
-
-pip install --upgrade pip setuptools
-pip install jaxlib==0.1.76 -f https://storage.googleapis.com/jax-releases/jax_releases.html
-pip install ./uncertainty-baselines[datasets,models]
