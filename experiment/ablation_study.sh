f7a8cc375bee35ddb0f46acd519b3b2849034900
mxrz1 <ranzinger.maximilian@gmail.com>
2024-12-13 11:55:39 +0100

    finished ablation study script

--- experiment/ablation_study.sh
+++ experiment/ablation_study.sh
@@ -42,40 +42,117 @@ python src/cifar/sgn_ablation.py --data_dir=$DATA_DIR \
     sbatch $SCRIPT_PATH
 }
 
-echo "============== DISABLE LR LC EP ================="
-echo "============== CIFAR10_SYM40_XXX ================"
-TEST_NAME=CIFAR10_SYM40_XXX
+echo "============== DISABLE LR LC ================="
+echo "============== CIFAR10_SYM40_XX ================"
+TEST_NAME=CIFAR10_SYM40_XX
 SCRIPT_NAME="$TEST_NAME.sh"
-submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type sym --severity 0.4 --disable_lr --alpha 1 --beta 0
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type sym --severity 0.4 --disable_lr --alpha 1
 
-echo "============== CIFAR10_ASYM40_XXX ================"
-TEST_NAME=CIFAR10_ASYM40_XXX
+echo "============== CIFAR10_ASYM40_XX ================"
+TEST_NAME=CIFAR10_ASYM40_XX
 SCRIPT_NAME="$TEST_NAME.sh"
-submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type asym --severity 0.4 --disable_lr --alpha 1 --beta 0
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type asym --severity 0.4 --disable_lr --alpha 1
 
-echo "============== CIFAR10N_RAND1_XXX ================"
-TEST_NAME=CIFAR10N_RAND1_XXX
+echo "============== CIFAR10N_RAND1_XX ================"
+TEST_NAME=CIFAR10N_RAND1_XX
 SCRIPT_NAME="$TEST_NAME.sh"
-submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type rand1 --disable_lr --alpha 1 --beta 0
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type rand1 --disable_lr --alpha 1
 
-echo "============== CIFAR10N_WORST_XXX ================"
-TEST_NAME=CIFAR10N_WORST_XXX
+echo "============== CIFAR10N_WORST_XX ================"
+TEST_NAME=CIFAR10N_WORST_XX
 SCRIPT_NAME="$TEST_NAME.sh"
-submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type worst --disable_lr --alpha 1 --beta 0
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type worst --disable_lr --alpha 1
 
-echo "============== CIFAR100_SYM40_XXX ================"
-TEST_NAME=CIFAR100_SYM40_XXX
+echo "============== CIFAR100_SYM40_XX ================"
+TEST_NAME=CIFAR100_SYM40_XX
 SCRIPT_NAME="$TEST_NAME.sh"
-submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar100 --noisy_labels --corruption_type sym --severity 0.4 --disable_lr --alpha 1 --beta 0
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar100 --noisy_labels --corruption_type sym --severity 0.4 --disable_lr --alpha 1
 
-echo "============== CIFAR100_ASYM40_XXX ================"
-TEST_NAME=CIFAR100_ASYM40_XXX
+echo "============== CIFAR100_ASYM40_XX ================"
+TEST_NAME=CIFAR100_ASYM40_XX
 SCRIPT_NAME="$TEST_NAME.sh"
-submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar100 --noisy_labels --corruption_type asym --severity 0.4 --disable_lr --alpha 1 --beta 0
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar100 --noisy_labels --corruption_type asym --severity 0.4 --disable_lr --alpha 1
 
-echo "============== CIFAR100N_XXX ================"
-TEST_NAME=CIFAR100N_XXX
+echo "============== CIFAR100N_XX ================"
+TEST_NAME=CIFAR100N_XX
 SCRIPT_NAME="$TEST_NAME.sh"
-submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar100 --noisy_labels --corruption_type c100noise --disable_lr --alpha 1 --beta 0
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar100 --noisy_labels --corruption_type c100noise --disable_lr --alpha 1
+
+
+echo "============== LX ================="
+echo "============== CIFAR10_SYM40_LX ================"
+TEST_NAME=CIFAR10_SYM40_LX
+SCRIPT_NAME="$TEST_NAME.sh"
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type sym --severity 0.4 --alpha 1
+
+echo "============== CIFAR10_ASYM40_LX ================"
+TEST_NAME=CIFAR10_ASYM40_LX
+SCRIPT_NAME="$TEST_NAME.sh"
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type asym --severity 0.4 --alpha 1
+
+echo "============== CIFAR10N_RAND1_LX ================"
+TEST_NAME=CIFAR10N_RAND1_LX
+SCRIPT_NAME="$TEST_NAME.sh"
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type rand1 --alpha 1
+
+echo "============== CIFAR10N_WORST_LX ================"
+TEST_NAME=CIFAR10N_WORST_LX
+SCRIPT_NAME="$TEST_NAME.sh"
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type worst --alpha 1
+
+echo "============== CIFAR100_SYM40_LX ================"
+TEST_NAME=CIFAR100_SYM40_LX
+SCRIPT_NAME="$TEST_NAME.sh"
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar100 --noisy_labels --corruption_type sym --severity 0.4 --alpha 1
+
+echo "============== CIFAR100_ASYM40_LX ================"
+TEST_NAME=CIFAR100_ASYM40_LX
+SCRIPT_NAME="$TEST_NAME.sh"
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar100 --noisy_labels --corruption_type asym --severity 0.4 --alpha 1
+
+echo "============== CIFAR100N_LX ================"
+TEST_NAME=CIFAR100N_LX
+SCRIPT_NAME="$TEST_NAME.sh"
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar100 --noisy_labels --corruption_type c100noise --alpha 1
+
+#echo "============== LL ================="
+#echo "get data from other runs"
+
+echo "============== XL ================="
+echo "============== CIFAR10_SYM40_XL ================"
+TEST_NAME=CIFAR10_SYM40_XL
+SCRIPT_NAME="$TEST_NAME.sh"
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type sym --severity 0.4 --disable_lr
+
+echo "============== CIFAR10_ASYM40_XL ================"
+TEST_NAME=CIFAR10_ASYM40_XL
+SCRIPT_NAME="$TEST_NAME.sh"
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type asym --severity 0.4 --disable_lr
+
+echo "============== CIFAR10N_RAND1_XL ================"
+TEST_NAME=CIFAR10N_RAND1_XL
+SCRIPT_NAME="$TEST_NAME.sh"
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type rand1 --disable_lr
+
+echo "============== CIFAR10N_WORST_XL ================"
+TEST_NAME=CIFAR10N_WORST_XL
+SCRIPT_NAME="$TEST_NAME.sh"
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar10 --noisy_labels --corruption_type worst --disable_lr
+
+echo "============== CIFAR100_SYM40_XL ================"
+TEST_NAME=CIFAR100_SYM40_XL
+SCRIPT_NAME="$TEST_NAME.sh"
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar100 --noisy_labels --corruption_type sym --severity 0.4 --disable_lr
+
+echo "============== CIFAR100_ASYM40_XL ================"
+TEST_NAME=CIFAR100_ASYM40_XL
+SCRIPT_NAME="$TEST_NAME.sh"
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar100 --noisy_labels --corruption_type asym --severity 0.4 --disable_lr
+
+echo "============== CIFAR100N_XL ================"
+TEST_NAME=CIFAR100N_XL
+SCRIPT_NAME="$TEST_NAME.sh"
+submit_job $TEST_NAME $SCRIPT_NAME --dataset cifar100 --noisy_labels --corruption_type c100noise --disable_lr
+
 
 echo "All jobs submitted."
